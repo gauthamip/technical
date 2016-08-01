21af6a7be0d2e57c85e0db4663888bd8b6fe920d
gauthamip <gauthamip26@gmail.com>
2016-08-01 07:40:37 +0530

    adding apps

--- TechnicalForum/settings.py
+++ TechnicalForum/settings.py
@@ -135,22 +135,35 @@ USE_TZ = True
 import dj_database_url
 DATABASES['default'] =  dj_database_url.config()
 
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+
+# STATIC_URL = '/static/'
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
-STATICFILES_DIRS=[
-    os.path.join(BASE_DIR,"static"),
-]
+# STATICFILES_DIRS=[
+#     os.path.join(BASE_DIR,"static"),
+# ]
 
-STATIC_ROOT=os.path.join(os.path.dirname(BASE_DIR),"static")
+# STATIC_ROOT=os.path.join(os.path.dirname(BASE_DIR),"static")
 
 MEDIA_URL='/media/'
 
-MEDIA_ROOT=os.path.join(os.path.dirname(BASE_DIR),"media")
+MEDIA_ROOT=os.path.join(PROJECT_ROOT, 'media')
 
 LOGIN_REDIRECT_URL='/tech/question/'
 
--- wsgi.py
+++ wsgi.py
@@ -8,15 +8,9 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
 import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "TechnicalForum.settings")
-
-# application = get_wsgi_application()
-
-from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
-
-application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
+# This application object is used by the development server
+# as well as any WSGI server configured to use this file.
+import django.core.handlers.wsgi
+application = django.core.handlers.wsgi.WSGIHandler()
